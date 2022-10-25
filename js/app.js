85e2bf92cdd8bc52068520a389d3d3063defeec9
juambeltz <juambeltzjuani@gmail.com>
2022-10-25 18:01:37 -0300

    carpetas

---
+++ js/app.js
@@ -0,0 +1,14 @@
+//const imagenes = document.querySelectorAll('.propiedad__imagen');
+
+//console.log(imagenes);
+
+//imagenes.forEach( (imagen) =>{
+    //console.log(imagen.style.backgroundPositionY);
+    //imagen.style.backgroundPositionY = '30px';
+//}  )
+
+//window.addEventListener('scroll', () => {
+   // alert('Dando scroll');
+   //console.log(this);
+   //console.log(this.scrollY);
+//})
